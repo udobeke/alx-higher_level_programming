868ea28c586333bbc69ad955c86668d065dc4158
Ubuntu <pchinagwam@gmail.com>
2023-04-03 21:08:57 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,13 @@
 """Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
+url = "https://alx-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
