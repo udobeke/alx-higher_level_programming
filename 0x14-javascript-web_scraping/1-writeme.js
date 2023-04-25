3355a3c92553f497c1ed4786b6ba2193a0144b4c
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-25 15:42:31 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,15 +2,10 @@
 /* a script that writes a string to a file. */
 
 const fs = require('fs');
-
-// Get file path and string to write from command line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
-
-// Write string to file in utf-8
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
-    // Print error object if error occurred during writing
     console.error(err);
  }
    // console.log(`Successfully wrote to file: ${filePath}`);
