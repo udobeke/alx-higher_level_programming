0a660e3961b8a1dac9a89f0e503e68066c5734a9
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-25 21:35:59 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,21 +3,9 @@
 the episode number matches a given integer. //
 
 const request = require('request');
-
-// Get movie ID from command line argument
-const url = process.argv[2];
-// Send GET request to SWAPI
-request(url, (err, response, body) => {
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (err, res, body) => {
   if (err) console.error(err);
   const data = JSON.parse(body);
-  let count = 0;
-  for (let i = 0; i < data.results.length; i++) {
-    const chars = data.results[i].characters;
-    for (let k = 0; k < chars.length; k++) {
-      if (chars[k].includes('18')) {
-        count = count + 1;
-      }
-    }
-  }
-  console.log(count);
+  console.log(data.title);
 });
