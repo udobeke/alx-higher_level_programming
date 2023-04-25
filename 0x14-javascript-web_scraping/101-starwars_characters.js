25541205d7e4b98eee2b3a0d72752758c6b3efb9
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-25 22:27:41 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,58 +1,35 @@
 #!/usr/bin/node
 //a script that prints all characters of a Star Wars movie
 
-
 const request = require('request');
-if (process.argv.length < 3) {
-  console.error('Error: Movie ID not provided');
-  console.log('Usage: node script.js <movie_id>');
-  process.exit(1);
-}
 
-const movieId = process.argv[2]; // Get movie ID from command line argument
-const apiUrl = `https://swapi.dev/api/films/${movieId}`; // API URL for movie details
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+let characters = [];
 
-request.get(apiUrl, (err, response, body) => {
-  if (err) {
-    // Print error if request encounters an error
-    console.error(err);
-  } else {
-    if (response.statusCode === 200) {
-      const movie = JSON.parse(body); // Parse response body as JSON
-
-      
-      console.log('Characters:');
-      const characterPromises = movie.characters.map(characterUrl => {
-        return new Promise((resolve, reject) => {
-          request.get(characterUrl, (err, response, body) => {
-            if (err) {
-              reject(err);
-            } else {
-              if (response.statusCode === 200) {
-                const character = JSON.parse(body); // Parse response body as JSON
-                resolve(character.name); // Resolve Promise with character name
-              } else {
-                reject(`Failed to fetch character details for URL ${characterUrl}: ${response.statusCode}`);
-              }
-            }
-          });
-        });
-      });
-
-      Promise.all(characterPromises)
-        .then(characterNames => {
-          characterNames.forEach(characterName => {
-            console.log(characterName); // Print character name
-          });
-        })
-        .catch(err => {
-          // Print error message if any Promise rejects
-          console.error(err);
-        });
-
-    } else {
-      // Print error message if response has a non-200 status code
-      console.error(`Failed to fetch movie details: ${response.statusCode}`);
-    }
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
+
+  const data = JSON.parse(body);
+  characters = data.characters;
+  getCharacters(0);
 });
+
+const getCharacters = (index) => {
+  if (index === characters.length) {
+    return;
+  }
+
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const characterData = JSON.parse(body);
+    console.log(characterData.name);
+    getCharacters(index + 1);
+  });
+};
