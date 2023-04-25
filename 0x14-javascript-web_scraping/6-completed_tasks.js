95dc981d16a20720a32c2f84f4694a2025340265
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-25 22:00:30 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,40 +1,19 @@
 #!/usr/bin/node
+// a script that prints all characters of a Star Wars movie
 
 const request = require('request');
-
-// API URL
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+const url = process.argv[2];
 
 // Send GET request to fetch todos data
-request.get(apiUrl, (err, response, body) => {
-  if (err) {
-    // Print error if request encounters an error
-    console.error(err);
-  } else {
-    if (response.statusCode === 200) {
-      const todos = JSON.parse(body); // Parse response body as JSON
-      const completedTasks = {}; // Object to store completed tasks count by user ID
-
-      // Loop through todos and count completed tasks by user ID
-      todos.forEach(todo => {
-        if (todo.completed) {
-          if (!completedTasks[todo.userId]) {
-            completedTasks[todo.userId] = 1;
-          } else {
-            completedTasks[todo.userId]++;
-          }
-        }
-      });
-
-      // Print completed tasks count by user ID
-      console.log('Completed tasks by user ID:');
-      for (const userId in completedTasks) {
-        console.log(`User ID: ${userId}, Completed tasks: ${completedTasks[userId]}`);
-      }
-    } else {
-      // Print error message if response has a non-200 status code
-      console.error(`Failed to fetch todos data: ${response.statusCode}`);
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+   const tasks = JSON.parse(body);
+  const dict = {};
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].completed === true) {
+      if (!dict[tasks[i].userId]) dict[tasks[i].userId] = 0;
+      dict[tasks[i].userId] += 1;
     }
   }
+  console.log(dict);
 });
-
