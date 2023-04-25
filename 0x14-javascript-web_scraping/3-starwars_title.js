d1f03d59602ee3126514ba691aea0549ef517619
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-25 22:32:04 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // A script that prints the title of a Star Wars movie where 
-the episode number matches a given integer. //
 
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
