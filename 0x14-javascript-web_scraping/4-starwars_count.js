bce399299c3408529571aa954ec05a135ae5cdeb
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-25 22:30:24 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // a script that prints the number of movies 
-where the character “Wedge Antilles” is present //
 
 const request = require('request');
 const url = process.argv[2];
