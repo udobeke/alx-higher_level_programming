89b8eb8f1932994e3657e0b2f8c85c67640a0186
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-25 21:29:24 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,14 +2,9 @@
 /* script to read files */
 
 const fs = require('fs');
-
-// Get file path from command line argument
 const filePath = process.argv[2];
-
-// Read file content in utf-8
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    // Print error object if error occurred during reading
     console.error(err);
     return;
   } 
