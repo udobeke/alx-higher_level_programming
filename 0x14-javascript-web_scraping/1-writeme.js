212d52200a4ba624774c69a19912041ddbff3896
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-26 21:46:36 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,9 +4,9 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
+
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
  }
-   // console.log(`Successfully wrote to file: ${filePath}`);
 });
