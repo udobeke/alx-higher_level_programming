88cbc40ece1e6884a3d6dcbf617ad4a35fa239b7
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-26 21:40:55 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,8 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
+  if (err) console.error(err);
     return;
-  } 
     // Print file content
     console.log(data);
 });
