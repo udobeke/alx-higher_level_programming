f82f3b518abbe5b69caa96e639e24345529d59c0
Peace Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-04-26 21:58:03 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,11 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, (err, res, body) => {
-  if (err) console.error(err);
-  const data = JSON.parse(body);
-  console.log(data.title);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
   }
 });
