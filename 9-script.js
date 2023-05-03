ff91c254d7ff3953a5f235fcad316833a1cc7192
Peace Chinagwam <pchinagwam@gmail.com>
2023-05-03 14:14:56 +0100

    tasks

--- 9-script.js
+++ 9-script.js
@@ -1,5 +1,5 @@
-$('document').ready(function () {
-  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+$(document).ready(function() {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function(data) {
     $('DIV#hello').text(data.hello);
   });
 });
